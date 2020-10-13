f37f3670eb1483137d03f43c4fb7631247986c7f
Rachel Powers <powers.e.rachel@gmail.com>
2020-10-13 17:25:11 -0600

    fix pika version

--- python_apps/airtime_analyzer/setup.py
+++ python_apps/airtime_analyzer/setup.py
@@ -29,8 +29,8 @@ setup(name='airtime_analyzer',
       packages=['airtime_analyzer'],
       scripts=['bin/airtime_analyzer'],
       install_requires=[
-          'mutagen==1.43.0', # 1.44.0 and above drops python 2 support
-          'pika',
+          'mutagen==1.43', # 1.44.0 and above drops python 2 support
+          'pika~=1.1.0',
           'daemon',
           'file-magic',
           'nose',
